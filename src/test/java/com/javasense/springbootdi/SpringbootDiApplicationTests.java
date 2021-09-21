c6fe5643acd9b119636f078bfa4dbcb8d56a96ce
omarismail95 <omar.pal.95@gmail.com>
2021-09-21 07:00:38 +0300

    my first commit

---
+++ src/test/java/com/javasense/springbootdi/SpringbootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasense.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
